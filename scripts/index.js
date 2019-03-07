5e6303028e47994babd92a7df5342448d3210f89
Matt Raines <mraines4@gmail.com>
2019-03-07 15:33:17 -0500

    adds ability to rotate through jokes

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,28 @@ const funnyJokes = [
     "I wouldn't buy anything with velcro. It's a total rip-off.",
     "The other day, my wife asked me to pass her lipstick but I accidentally passed her a glue stick. She still isn't talking to me."
 ]
+
+const jokeHolder = document.querySelector('[data-container');
+const buttonPress = document.querySelector('[data-button]')
+
+// When the user clicks the button, show a joke/fortune.
+let i = 0;
+
+function nextJoke () {
+    jokeHolder.textContent = funnyJokes[i];
+    i += 1;
+    if (i === funnyJokes.length) {
+        i = 0;
+    }
+}
+
+buttonPress.addEventListener('click', nextJoke)
+
+
+// When the user clicks the button again, show the next fortune.
+
+
+
+// When the user clicks the button, and you are at the end of the array, start back from the beginning of the array.
+
+
