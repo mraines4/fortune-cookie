8032e56cb48b786d6dbf3d320e03f6c6b4cf2ac8
Matt Raines <mraines4@gmail.com>
2019-03-07 14:34:23 -0500

    adds dad jokes variable

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const funnyJokes = [
+    "I hate perforated lines, they're tearable.",
+    "Parallel lines have so much in common. It’s a shame they’ll never meet.",
+    "My first time using an elevator was an uplifting experience. The second time let me down.",
+    "They're making a movie about clocks. It's about time",
+    "I wouldn't buy anything with velcro. It's a total rip-off.",
+    "The other day, my wife asked me to pass her lipstick but I accidentally passed her a glue stick. She still isn't talking to me."
+]
