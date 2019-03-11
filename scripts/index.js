7c94269453325cfa1e74f0deb7a9732f0039ab82
Matt Raines <mraines4@gmail.com>
2019-03-11 15:40:02 -0400

    adds fetch - using fortunes

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-const funnyJokes = [
-    "I hate perforated lines, they're tearable.",
-    "Parallel lines have so much in common. It’s a shame they’ll never meet.",
-    "My first time using an elevator was an uplifting experience. The second time let me down.",
-    "They're making a movie about clocks. It's about time",
-    "I wouldn't buy anything with velcro. It's a total rip-off.",
-    "The other day, my wife asked me to pass her lipstick but I accidentally passed her a glue stick. She still isn't talking to me."
-]
+// const zeJokes = [
+//     "I hate perforated lines, they're tearable.",
+//     "Parallel lines have so much in common. It’s a shame they’ll never meet.",
+//     "My first time using an elevator was an uplifting experience. The second time let me down.",
+//     "They're making a movie about clocks. It's about time",
+//     "I wouldn't buy anything with velcro. It's a total rip-off.",
+//     "The other day, my wife asked me to pass her lipstick but I accidentally passed her a glue stick. She still isn't talking to me."
+// ]
 
 const jokeHolder = document.querySelector('[data-container');
 const buttonForward = document.querySelector('[data-button-next]');
@@ -18,11 +18,11 @@ const buttonRandom = document.querySelector('[data-button-rand]');
 
 let i = -1;
 function nextJoke () {
-    if (i === funnyJokes.length - 1) {
+    if (i === zeJokes.length - 1) {
         i = -1;
     }
     i += 1
-    jokeHolder.textContent = funnyJokes[i];
+    jokeHolder.textContent = zeJokes[i];
 
     console.log(i);
 
@@ -30,23 +30,32 @@ function nextJoke () {
 function prevJoke () {
     i -= 1
     if ( i < 0) {
-        i = (funnyJokes.length - 1);
+        i = (zeJokes.length - 1);
     }
-    jokeHolder.textContent = funnyJokes[i];
+    jokeHolder.textContent = zeJokes[i];
     console.log(i);
 
 }
 
 function randJoke () {
-    let rando = Math.floor(Math.random() * Math.floor(funnyJokes.length));
-    jokeHolder.textContent = funnyJokes[rando];
+    let rando = Math.floor(Math.random() * Math.floor(zeJokes.length));
+    jokeHolder.textContent = zeJokes[rando];
     i = rando;
     console.log(i);
-
 }
 
 buttonForward.addEventListener('click', nextJoke);
 buttonBackward.addEventListener('click', prevJoke);
 buttonRandom.addEventListener('click', randJoke);
 
-
+let zeJokes = [] ;
+for (let i = 0; i < 10; i++) {
+    fetch('https://my-little-cors-proxy.herokuapp.com/http://yerkee.com/api/fortune')
+        .then(function(r) { return r.json() })
+        .then(function(jokez) {
+            // console.log(jokez);
+            zeJokes.push(jokez.fortune)
+        });
+    }
+    
+    console.log(zeJokes)
